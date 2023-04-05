2f89a14a57ab94b3f873092912b6e901fffb6f35
eagobert <eagobert@hotmail.com>
2023-04-05 19:37:00 -0400

    Update contact pg icon navigation

---
+++ force-app/main/default/aura/portfolioContactLayout/portfolioContactLayoutController.js
@@ -0,0 +1,54 @@
+({
+	/*Sets page reference on initialization*/
+    doInit : function(cmp, evt, helper){
+        //Set Page Reference
+        var pageRef = {
+            type: 'comm__namedPage',
+            attributes: {
+                componentName: 'portfolioContact__c',
+            }
+        };
+        cmp.set("v.pageReference", pageRef);
+    },
+    
+    /*Handles navigation on button click*/
+    handleClick : function(cmp, evt) {
+        var pageRef = cmp.get("v.pageReference");
+        var btnName = evt.getSource().get("v.name");
+        console.log("Button Name: " + btnName);
+        var navService = cmp.find("navService");
+        var pagRef = cmp.get("v.pageReference");
+        
+        switch (btnName){
+        /* If name is 'linkedIn' navigate to linkedIn site*/
+            case "linkedIn":
+                pageRef = {
+                    type: 'standard__webPage',
+                    attributes: {
+                        url: '',
+                    }
+                };
+                break;
+        /* If name is 'github' navigate to github site*/
+            case "github":
+                pageRef = {
+                    type: 'standard__webPage',
+                    attributes: {
+                        url: 'https://github.com/eagobert',
+                    }
+                };
+                break;
+         /* If name is 'trailhead' navigate to trailhead site*/
+            case "trailhead":
+                pageRef = {
+                    type: 'standard__webPage',
+                    attributes: {
+                        url: 'https://trailblazer.me/id/erinagobert',
+                    }
+                };
+                break;
+            default:
+                console.log("No page reference for navigation.");
+		}
+	}
+})
